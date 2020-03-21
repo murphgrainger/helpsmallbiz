a5558ca5f4e8fc3820c991afb0ac3dd3cf3c082e
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2020-03-21 10:35:05 -0600

    working on homepage design" "

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,36 @@
 import React from 'react';
 
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
-
 
 function Home() {
   return (
-    <>
+    <div className="Home">
+    <Container maxWidth="lg">
+    <Grid container spacing={3}>
+    <Grid item xs={6} className="home-content">
       <header className="App-header">
       <h1>Pledge 10,000 Colorado</h1>
-      <h2>Support Colorado Local Businesses</h2>
+      <h3>Support Colorado Local Businesses During COVID-19 Restrictions</h3>
+      <p>Log a way you have supported a local business - any business! If we reach $10,000 of support the creators of this app will match $10,000.  All amounts go directly to local businesses with no profit for any third-party in mind.  This is a bootstrapped effort that trusts your honesty of the amounts you've submitted.</p>
+      <h4>How It Works</h4>
       <ul>
-        <li>Add or select a business to support</li>
+        <li>Choose a business to log your support</li>
         <li>Pledge an amount</li>
-        <li>Buy gift cards or donate via Venmo</li>
+        <li>Select how you supported them: donation, gift card, takeout / delivery / online order.</li>
         <li>See who others are donating to</li>
       </ul>
       </header>
-      <div class="button-wrapper">
+      <div className="button-wrapper">
         <Button variant="contained" color="secondary" href="/support-a-business">Support a Business</Button>
       </div>
-    </>
+    </Grid>
+      <Grid item xs={6} className="side-bar-image">
+    </Grid>
+    </Grid>
+    </Container>
+    </div>
   )
 }
 
--- src/pages/Support.js
+++ src/pages/Support.js
@@ -6,23 +6,28 @@ import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
-import Checkbox from '@material-ui/core/Checkbox';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
 import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import SearchIcon from '@material-ui/icons/Search';
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import Button from '@material-ui/core/Button';
+import Switch from '@material-ui/core/Switch';
 
 function Home(props) {
-
+  const [value, setValue] = React.useState('female');
   const [state, setState] = React.useState({
-     giftCard: true,
-     donation: false,
-     takeout: false,
-   });
+      checkedA: false,
+    });
 
   const handleChange = event => {
+    setValue(event.target.value);
+  };
+
+  const handlePrivacySelect = event => {
   setState({ ...state, [event.target.name]: event.target.checked });
 };
-
-const { giftCard, donation, takeout } = state;
-const error = [giftCard, donation, takeout].filter(v => v).length !== 2;
 
 
   return (
@@ -38,31 +43,42 @@ const error = [giftCard, donation, takeout].filter(v => v).length !== 2;
       </header>
 
       <form className="support-form" noValidate autoComplete="off">
-        <div style={{"width":"100%"}}>
-        <Autocomplete/>
-        </div>
-        <div className="donation-types">
-        <FormControl component="fieldset" >
-          <FormLabel component="legend">How Did You Donate</FormLabel>
-          <FormGroup>
-            <FormControlLabel
-              control={<Checkbox checked={giftCard} onChange={handleChange} name="giftCard" />}
-              label="Gift Card"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={donation} onChange={handleChange} name="donation" />}
-              label="Donation"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={takeout} onChange={handleChange} name="takeout" />}
-              label="Takeout / Delivery"
-            />
-          </FormGroup>
-        </FormControl>
-        </div>
-        <div className="donation-types">
+      <Grid container spacing={1} alignItems="center">
+       <Grid item>
+         <SearchIcon fontSize="large" xs={1}/>
+       </Grid>
+       <Grid item xs={10}>
+       <Autocomplete/>
+        </Grid>
+      </Grid>
+      <Grid container spacing={1} alignItems="center">
+       <Grid item>
+         <AttachMoneyIcon fontSize="large"/>
+       </Grid>
+       <Grid item>
         <TextField id="standard-basic" variant="outlined" label="Amount" fullWidth/>
-        </div>
+       </Grid>
+     </Grid>
+     <Grid container spacing={1} alignItems="center">
+      <FormControl component="fieldset">
+        <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+          <FormControlLabel value="female" control={<Radio />} label="Cash Donation" />
+          <FormControlLabel value="male" control={<Radio />} label="Gift Card" />
+          <FormControlLabel value="other" control={<Radio />} label="Takeout / Delivery / Online Order" />
+        </RadioGroup>
+      </FormControl>
+      </Grid>
+      <div className="form-footer">
+        <Grid container spacing={1} alignItems="center">
+          <FormControlLabel
+           control={<Switch checked={state.checkedA} onChange={handlePrivacySelect} name="checkedA" />}
+           label="Make My Support Anonymous"
+         />
+       </Grid>
+       <Grid container spacing={1} alignItems="center">
+        <Button variant="contained" color="secondary" href="/support-a-business">Submit Support</Button>
+        </Grid>
+      </div>
       </form>
     </>
   )
