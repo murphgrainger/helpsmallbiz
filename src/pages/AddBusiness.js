1d15a54c952597d4176c16935bbdf8612bf770ef
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2020-03-22 14:59:15 -0600

    added box margins

--- src/pages/AddBusiness.js
+++ src/pages/AddBusiness.js
@@ -6,14 +6,17 @@ import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
+import Checkbox from '@material-ui/core/Checkbox';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import SearchIcon from '@material-ui/icons/Search';
+import InfoIcon from '@material-ui/icons/Info';
 import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
 import Button from '@material-ui/core/Button';
 import Switch from '@material-ui/core/Switch';
+import Tooltip from '@material-ui/core/Tooltip';
+import Box from '@material-ui/core/Box';
+
 
 import { GOOGLE_API_KEY } from '../constants';
 
@@ -28,18 +31,20 @@ super(props);
     lastName: "",
     email: "",
     description: "",
-    challenge: ""
+    challenge: "",
+    terms: false
     }
 
 }
 
 
 handleChange = (event) => {
-  this.setState({[event.target.name]:event.target.value});
+  const value = event.target.name === 'terms'? !this.state.terms : event.target.value;
+  console.log(event.target);
+  this.setState({[event.target.name]:value});
 };
 
 setLocationValue = (value) => {
-  console.log(value);
   this.setState({
     businessName: value.structured_formatting.main_text,
     businessAddress: value.structured_formatting.secondary_text
@@ -58,15 +63,8 @@ return (
   <div className="form-wrapper">
     <header className="form-header">
       <h1>Add a Business</h1>
-      <ol>
-        <li>Find a Business to Support</li>
-        <li>Say Who You Are</li>
-        <li>Describe why you chose this business.</li>
-        <li>Create a goal for how you want to support them.</li>
-      </ol>
     </header>
-
-    <form className="support-form" noValidate autoComplete="off" onSubmit={this.onSubmit} >
+    <form className="support-form" autoComplete="off" onSubmit={this.onSubmit} >
       <Grid container spacing={1} alignItems="center">
         <Grid item xs={12}>
           <Autocomplete required setValue={this.setLocationValue} />
@@ -79,7 +77,9 @@ return (
             <TextField required id="standard-basic" multiline rowsMax="2" variant="outlined" name="challenge" label="Your Challenge" fullWidth onChange={this.handleChange}/>
           </Grid>
         </Grid>
-        <p>Your Information</p>
+        <p>Your Information &nbsp;</p>
+        <Tooltip title="Your name will be displayed publicly attached to this business. Your email will solely be used by the administrator to confirm your progress towards your goal." aria-label="add" placement="right-start"><InfoIcon color="primary" fontSize="small"/></Tooltip>
+
         <Grid container spacing={1} alignItems="center">
           <Grid item xs={6}>
             <TextField required id="standard-basic" variant="outlined" label="First Name" name="firstName" fullWidth onChange={this.handleChange}/>
@@ -89,14 +89,23 @@ return (
           </Grid>
         </Grid>
         <Grid item xs={12}>
-          <TextField required id="standard-basic" variant="outlined" label="Email" name="email" fullWidth onChange={this.handleChange}/>
+          <TextField required id="standard-basic" variant="outlined" type="email" label="Email" name="email" fullWidth onChange={this.handleChange}/>
         </Grid>
       </Grid>
 
       <div className="form-footer">
-        <Grid container spacing={1} alignItems="center">
-          <Button type="submit" variant="contained" color="secondary">Add Business</Button>
+      <Grid container spacing={1} alignItems="center" justify="center">
+        <FormControlLabel
+        control={<Checkbox checked={this.state.terms} onChange={this.handleChange} name="terms" />}
+        label="I understand my email will not be distributed or displayed and only used by the administrator of this app to verify my submission and completion of my goal."
+
+        />
+      </Grid>
+      <Box mt={2}>
+        <Grid container spacing={1} alignItems="center" justify="center" >
+          <Button className="submit-button" type="submit" variant="contained" color="secondary">Add Business</Button>
         </Grid>
+      </Box>
       </div>
     </form>
   </div>
