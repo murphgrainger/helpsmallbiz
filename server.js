020edeca080b59df1071d80f37cdf1057744de78
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2020-03-24 13:12:21 -0600

    removed babel script for now

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ const business = require('./business');
 
 const port = process.env.PORT || 8080;
 
-
-if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-  require('babel-register')({
-    presets: [ 'es2015' ]
-  });
-}
 const app = express();
 
 app.use(bodyParser.json())
