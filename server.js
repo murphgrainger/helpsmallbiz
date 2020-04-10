078f7c1e5540315d678ab97f871633020c6334ea
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2020-04-10 15:24:09 -0600

    fixed robot.txt file

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.get('/robots.txt', (req, res, next) => {
   res.type('text/plain');
   if (environment !== 'production') {
     res.send('User-agent: *\nDisallow: /');
+  } else {
+    res.send('User-agent: *')
   }
 });
 
